8008c52b47841cb927e4cb982802b0a56d319941
Afin Asgharli <afinasgharli@gmail.com>
2022-07-03 17:42:18 +0400

    Accordion Project title

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css';
+import {TestModule} from '../utils';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
